03cda8baf74d9a4ad0338f185237ea69c1bdcaeb
Alastair Carson <alastair.carson@thinkwhere.local>
2016-03-25 10:16:44 +0000

    proxy on wfs working

--- pythonproxy.py
+++ pythonproxy.py
@@ -5,11 +5,13 @@ import urllib
 PORT = 8000
 
 class Foo(SimpleHTTPServer.SimpleHTTPRequestHandler):
-#    def __init__(self,*args):
-#        SimpleHTTPServer.SimpleHTTPRequestHandler.__init__(args)
-
     def do_GET(self):
-        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+        if self.path.lower().find("wfs") != -1:
+            self.path = self.path[5:]
+            print self.path
+            self.copyfile(urllib.urlopen(self.path), self.wfile)
+        else:
+            SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
 
 httpd = SocketServer.TCPServer(('', PORT), Foo)
 print "serving at port", PORT
